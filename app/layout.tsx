f101c318526723bc44eedf6cf453b2c51f5298d6
Apurva Mane <apurvam944@gmail.com>
2025-04-14 20:05:38 +0530

    Fix line endings according to .gitattributes

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import React from "react";
+import {Toaster} from "sonner";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "PrepWise",
+  description: "An AI-Powered platform for preparing for mock interview",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body
+        className={`${monaSans.className} antialiasing pattern`}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
