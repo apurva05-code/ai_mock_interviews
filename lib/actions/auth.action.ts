453b2929bfc1930e3160b2f5a404fa3b039f5c20
Apurva Mane <apurvam944@gmail.com>
2025-05-01 18:22:09 +0530

    implement interview session

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -2,7 +2,7 @@
 
 import {db, auth} from "@/firebase/admin";
 import {cookies} from "next/headers";
-import {doc} from "@firebase/firestore";
+
 
 const ONE_WEEK = 60 * 60 * 24 * 7;
 
@@ -118,26 +118,3 @@ export async function isAuthenticated() {
     return !!user;
 }
 
-export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
-    const interviews = await db.collection('interviews').where('userId', '==', userId).orderBy('createdAt', 'desc').get();
-
-    return interviews.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-    })) as Interview[];
-
-
-}
-
-export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
-    const { userId, limit = 20 } = params;
-
-    const interviews = await db.collection('interviews').orderBy('createdAt', 'desc').where('finalized', '==', true).where('userId', '!=', userId).limit(limit).get();
-
-    return interviews.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-    })) as Interview[];
-
-
-}
